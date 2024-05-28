f186dfbc733a7362f2fda89da8b40801bd9f6b39
Vananh1551998 <vananh.l@glasshouseventure.studio>
2024-05-28 17:23:35 +0700

    Van anh lam BTVN lan 2

--- tests/vananh/study.spec.ts
+++ tests/vananh/study.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('SELECTOR COURSE 1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.locator('a.navbar__item[href="/docs/intro"]').click();
-
+  await page.locator('a.navbar__item[href="/docs/intro"]').click();
+  await page.locator('a.navbar__item[href="/docs/intro"]').click();
 })
 
