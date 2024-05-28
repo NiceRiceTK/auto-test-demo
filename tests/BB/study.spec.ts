d5f5ec1f938b14eec60a06972a578751270a6a01
Binh Bao <dinh.h@glasshouseventure.studio>
2024-05-28 21:53:14 +0700

    add step click login, set timeout and fill text into textinput

--- tests/BB/study.spec.ts
+++ tests/BB/study.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { defineConfig } from '@playwright/test';
 import { TIMEOUT } from 'dns';
+import { config } from 'process';
 
 test('SELECTOR COURSE 1', async ({ page }) => {
   await page.goto('https://www.prism.horse/');
   await page.getByText('login').click();
-  test.setTimeout(12000);
+  await page.waitForTimeout(8000);
+  await page.getByPlaceholder("username").fill('ft.trainer7');
+  // await page.inputValue("ft.trainer7");
+  await page.waitForTimeout(4000);
   //await page.locator('a.navbar__item[href="/docs/intro"]').click();
 })
 
