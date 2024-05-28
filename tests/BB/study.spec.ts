acb201383344fa3313f8b3e8ea2bdd44ab176681
Binh Bao <dinh.h@glasshouseventure.studio>
2024-05-28 21:58:29 +0700

    no message

--- tests/BB/study.spec.ts
+++ tests/BB/study.spec.ts
@@ -8,7 +8,6 @@ test('SELECTOR COURSE 1', async ({ page }) => {
   await page.getByText('login').click();
   await page.waitForTimeout(8000);
   await page.getByPlaceholder("username").fill('ft.trainer7');
-  // await page.inputValue("ft.trainer7");
   await page.waitForTimeout(4000);
   //await page.locator('a.navbar__item[href="/docs/intro"]').click();
 })
