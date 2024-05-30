9624a34a33c7682b5d72e9783eac932d39dbaf25
MacUser <>
2024-05-30 12:17:47 +0700

    BTVN

--- tests/CuongDN/BTVN1.spec.ts
+++ tests/CuongDN/BTVN1.spec.ts
@@ -2,6 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('BTVN1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await page.locator('a.navbar__item[href="/docs/intro"]').click();
+
+   /* dung css*/
+  await page.locator('a.getStarted_Sjon').click();
+  await page.locator('.buttons_pzbO > a[href="/docs/intro"]').click();
+  await page.locator('div.buttons_pzbO').click();
+  
  
 })
