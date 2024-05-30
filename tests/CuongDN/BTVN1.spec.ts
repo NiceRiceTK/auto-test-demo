d6a256b484a38b6953557d0d4f0eeb3257bc0341
MacUser <>
2024-05-30 17:39:24 +0700

    Nop BTVN hoan chinh

--- tests/CuongDN/BTVN1.spec.ts
+++ tests/CuongDN/BTVN1.spec.ts
@@ -3,10 +3,19 @@ import { test, expect } from '@playwright/test';
 test('BTVN1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-   /* dung css*/
+   // dung css
   await page.locator('a.getStarted_Sjon').click();
   await page.locator('.buttons_pzbO > a[href="/docs/intro"]').click();
   await page.locator('div.buttons_pzbO').click();
+  await page.locator('a.getStarted_Sjon[href="/docs/intro"]').click();
+  //Xpath
+  await page.locator('//a[@class="getStarted_Sjon"]').click();
+  await page.locator('//div[@class="buttons_pzbO"]/a[@href="/docs/intro"]').click();
+  await page.locator('//a[contains(text(),"Get started")]').click();
+
+  // Playwright
+  await page.getByRole('link', { name: 'GET STARTED' }).click();
+
   
  
 })
