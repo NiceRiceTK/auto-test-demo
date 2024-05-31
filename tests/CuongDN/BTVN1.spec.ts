73cf6eec659535978361c933765a9a8c21ca8fc1
son.nguyen <son.nguyen@bluebottle.digital>
2024-05-31 09:53:09 +0700

    Checking CuongDN/BTVN1

--- tests/CuongDN/BTVN1.spec.ts
+++ tests/CuongDN/BTVN1.spec.ts
@@ -5,12 +5,12 @@ test('BTVN1', async ({ page }) => {
 
    // dung css
   await page.locator('a.getStarted_Sjon').click();
-  await page.locator('.buttons_pzbO > a[href="/docs/intro"]').click();
+  await page.locator('.buttons_pzbO > a[href="/docs/intro"]').click(); // Shorten to .buttons_pzbO > a
   await page.locator('div.buttons_pzbO').click();
   await page.locator('a.getStarted_Sjon[href="/docs/intro"]').click();
   //Xpath
   await page.locator('//a[@class="getStarted_Sjon"]').click();
-  await page.locator('//div[@class="buttons_pzbO"]/a[@href="/docs/intro"]').click();
+  await page.locator('//div[@class="buttons_pzbO"]/a[@href="/docs/intro"]').click(); // Shorten to //div[@class="buttons_pzbO"]/a
   await page.locator('//a[contains(text(),"Get started")]').click();
 
   // Playwright
@@ -18,5 +18,5 @@ test('BTVN1', async ({ page }) => {
   await page.getByText('Get started').click();
 
   
- 
+ // Great job, no buts. A+
 })
