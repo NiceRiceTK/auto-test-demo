a150d3ff34ce9403bfebc90559bc183083e4f2c3
ADMIN-PC\ADMIN <cuong.d@glasshouseventure.studio>
2024-06-01 00:23:12 +0700

    BTVN2

--- tests/CuongDN/BTVN2.spec.ts
+++ tests/CuongDN/BTVN2.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-
 test('BTVN2', async ({ page }) => {
   
   const A = {
@@ -14,13 +13,26 @@ test('BTVN2', async ({ page }) => {
     A8:"3",
     A9:"3"
   };
+
+  const B =[1,2,3,4,5,6,1,2,3,4];
+  
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log('The sum of the first 10 natural numbers:', sum);
+
+
+});
+
+test('BTVN22', async ({ page }) => {
+  
   const B =[1,2,3,4,5,6,1,2,3,4];
 
-  let C:number = 0;
-
-   for (let i = 0 ; i <= B.length; i++){
-     C = C + B[i];
-   }
-    console.log('calculate the sum of the first 10 natural numbers: ' + C);
+let C:number = 0;
+for (let i = 0 ; i < B.length; i++){
+  C += B[i];
+}
+console.log('calculate the sum of the first 10 element of the arrary: ' + C);
 
 })
