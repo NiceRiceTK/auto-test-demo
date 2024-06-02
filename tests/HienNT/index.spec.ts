e383850213397571f148ab112ee202601ad47339
HienNT2000 <hien.nt@glasshouseventure.studio>
2024-06-03 00:48:53 +0700

    Completed BTVN

--- tests/HienNT/index.spec.ts
+++ tests/HienNT/index.spec.ts
@@ -3,31 +3,17 @@ import { test, expect } from '@playwright/test';
 test('Hien DEMO', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  /* Clicking Docs link in nav bar.
-    This part   a.navbar__item   will search for any <a> tags that HAVE 'navbar__item' in their 'class' attribute.
-    This part   [href="/docs/intro"]   is used to filter the result of the part above, in this case it will
-    only show any <a> tag that have 'docs/intro' in their 'href' attribute
-  */
-  await page.locator('a.navbar__item[href="/docs/intro"]').click();
+  //  CSS
+  await page.locator('a.navbar__brand').click();
+  await page.locator('.gh-btn[href$="/playwright"]').click();
+  await page.locator('button.DocSearch').click();
 
-  /* Clicking Docs link in nav bar.
-    This part   [href$="welcome"]   will search for any tags that have their 'href' attribute end with 'welcome'.
-    If you want to switch to begin with instad of ending with then change '$' to '^'
-  */
-  await page.locator('[href$="welcome"]').click();
 
-  /* Clicking Docs link in nav bar.
-    This part   h2#ambassadors   will search for any <h2> tags that have their 'id' attribute equal to 'ambassadors'.
-  */
-    await page.locator('h2#ambassadors').click();
+  // // // Xpath
+  await page.locator('//a[@class="navbar__brand"]').click();
+  await page.locator('//a[contains(text(),"JavaScript")]').click();
+  
+  await page.locator('//ul[@class="dropdown__menu"]/li/a[@href="/java/"]').click();
 
-  /* To summerize,
-     An element can be located with CSS selector 
-        by its attributes like 'href', 'type, 'label', etc through the use of [] brackets, 
-        by its 'class' attribute with the use of the . character
-        by its 'id' attribute with the use of the # character.
-     And you can partial match the attributes with the use of 
-        '^' for begining and
-        '$' for ending
-  */
+  
 });
