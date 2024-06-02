a7378ff792e80dbd2b6533bb90b438bf1a2d1810
thaothao16 <thao.l@glasshouseventure.studio>
2024-06-03 03:19:46 +1000

    Complete BTVN

---
+++ tests/ThaoLe/BTVNThao.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('BTVNThao', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+
+   //css
+  await page.locator('.navbarSearchContainer_Bca1>button').click(); 
+  await page.locator('.DocSearch').click();
+  
+
+   //Xpath
+   await page.locator('//button[@class="DocSearch DocSearch-Button"]').click();
+  //  await page.locator('//button[@class="DocSearch-Input"]').click();
+  //  await page.locator('//button[@class="DocSearch-Input"]').fill('git');
+  //  await page.waitForTimeout(3000);
+  //  await page.locator('//button[@class="DocSearch-Reset"]').click();
+ 
+
+
+   
+});
