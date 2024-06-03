d2241f5b3bd1e98065e53e1a73bf79550e3991b8
DESKTOP-5IGMGNJ\hienn <hien.nt@glasshouseventure.studio>
2024-06-03 09:21:37 +0700

    Final update

--- tests/HienNT/index.spec.ts
+++ tests/HienNT/index.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('Hien DEMO', async ({ page }) => {
+test('CSS Locator', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  //  CSS
   await page.locator('a.navbar__brand').click();
   await page.locator('.gh-btn[href$="/playwright"]').click();
   await page.locator('button.DocSearch').click();
 
-  // Xpath
+});
+
+test('Xpath', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
   await page.locator('//a[@class="navbar__brand"]').click();
   await page.locator('//a[contains(text(),"JavaScript")]').click();
   await page.locator('//ul/li[@class="footer__item"]/a[@href="/docs/intro"]').click();
