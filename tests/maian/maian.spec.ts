16269b0197a2356e466560a843bd11413d557e5a
son.nguyen <son.nguyen@bluebottle.digital>
2024-06-04 15:31:15 +0700

    LESSON 2

--- tests/maian/maian.spec.ts
+++ tests/maian/maian.spec.ts
@@ -5,12 +5,12 @@ test('CSS SELECTOR DEMO', async ({ page }) => {
 
   await page.locator('//div[@class="navbar__items"]//a[3]').click();
 
-  await page.locator('//a[@class="navbar__item navbar__link" and @href=\'/docs/api/class-playwright\']').click();
+  await page.locator('//a[@class="navbar__item navbar__link" and @href="docs/api/class-playwright"]').click();
 
   await page.locator('//a[text()="API"]').click();
 
   await page.locator('//div[@class="navbar__inner"]//a[@href="/docs/api/class-playwright"]').click();
 
-  await page.locator('//html/body/div[1]/nav/div[1]/div[1]/a[2]').click();
+  await page.locator('//html/body/div[1]/nav/div[1]/div[1]/a[2]').click(); 
 
 });
