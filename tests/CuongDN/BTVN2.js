34ba47a3a294c7289246b593d9ab3eb14d6fade7
MacUser <>
2024-06-07 15:40:43 +0700

    Update js BTVN2

--- tests/CuongDN/BTVN2.js
+++ tests/CuongDN/BTVN2.js
@@ -1,11 +1,11 @@
-const readline = require('node:readline');
+const readline = require('node:readline');// khai bao readLine tham chieu den module node:readline
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+}); //  khai bao 1 bien dung de nhan lieu vao va tra ket qua ra
 
-rl.question('Enter a positive integer:', name => {
+rl.question('Enter a positive integer:', name => { //question()  duoc su dung de dat cau hoi va in ra cau tra loi
     let sum = 0
     for (let i = 1; i <= parseInt(name); i++) {
         sum += i;
