197b65cc52dce76d014b8135f216036c59ea8815
quynhgvs <quynh.h@glasshouseventure.studio>
2024-06-14 17:53:28 +0700

    no message

--- tests/QuynhGVS/playwright.spec.ts
+++ tests/QuynhGVS/playwright.spec.ts
@@ -1,24 +1,18 @@
 import { test, expect } from '@playwright/test';
-import { login } from './playwright';
 
-test('test name', async ({ page }) => { 
-    await page.goto('https://playwright.dev/docs/input');
-    //await page.getByText('These are the recommended built in locators.');
-    // await page.locator('//*[contains(@class,"DocSearch")]').nth(0).click();
-    await page.locator('//button[contains(@class,"DocSearch")]').click();
-
-    await page.getByPlaceholder('Search docs').fill('action');
-    // await page.getByText('CI GitHub').click();
-    // await page.getByTestId('docsearch-item-5').click();
-    await page.getByRole('link', { name: 'Setting up GitHub Actions​ CI' }).click()
-
-    // expect(page.getByText('CI GitHub Actions', {exact: true})).toBeVisible();
-    await expect(page.locator('//h1[text() = "CI GitHub Actions"]')).toBeVisible();
-
-    const title = await page.locator('//h1').textContent();
-    expect(title).toEqual('CI GitHub Actions');
+test('Create Post', async ({ page }) => { 
+    test.slow(); // Easy way to triple the default timeout
+    await page.goto('https://samsungfood.com/');
+    await page.getByRole ('link', {name: 'Log In'}).click();
+    await page.getByTestId('auth-continue-with-email-button').click();
+    await page.getByPlaceholder('Email or phone number').fill('hathithuyquynh98.com@gmail.com');
+    await page.getByTestId('auth-continue-button').click();
+    await page.getByPlaceholder('Password').fill('ManGo@33');
+    await page.getByTestId('auth-login-button').click();
+    await page.getByTestId('4ba90cc7-7c54-b96c-ccfd-93904139acad').click();
+    await page.getByTestId('0d6281db-9f5a-8f63-c0d4-c40f91221d00').click();
+    await page.getByTestId('profile').click();
+    await page.getByPlaceholder('Add text (optional)').fill('Hello :)))');
+    await page.getByTestId('65358485-2a33-d3b7-c5ce-a36ccec6be4e').click();
+    await expect(page.getByTestId('6ba614ad-462f-714f-65b5-5c1f99689c37')).toContainText('Posted successfully!');
 });  
-
-test('Using javascript in playwright', async (page) => {
-    login (page)
-};)
